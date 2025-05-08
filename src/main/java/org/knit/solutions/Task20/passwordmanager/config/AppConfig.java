9350a93c822697fd0fa22441605b8ee007b72148
Mostov <vladimircev327@gmail.com>
2025-05-08 20:26:35 +0400

    Лабораторная работа 2.7: Password Manager с Spring и шифрованием

---
+++ src/main/java/org/knit/solutions/Task20/passwordmanager/config/AppConfig.java
@@ -0,0 +1,9 @@
+package org.knit.solutions.Task20.passwordmanager.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.passwordmanager")
+public class AppConfig {
+}
